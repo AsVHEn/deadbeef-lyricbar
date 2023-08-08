6ff373509b6d4c25ff0f0b873622452dcb7c1290
AsVHEn <asvhen@gmail.com>
2023-08-08 13:26:11 +0200

    Delete config_dialog.h

--- config_dialog.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <gtk/gtk.h>
-
-
-void on_button_config (GtkMenuItem *menuitem, gpointer user_data);
-void on_button_search (GtkMenuItem *menuitem, gpointer user_data);
-void on_button_edit (GtkMenuItem *menuitem, gpointer user_data);
